2eefb6225723db82a5f59044508e8a6f429f5a1a
Moose <yugihanmaster321@gmail.com>
2024-02-18 22:04:03 -0500

    cage setup

---
+++ Assets/Scripts/Cage Scripts/FollowPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [Header("Following Player")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private float stopDist;
+    [SerializeField] private float speed;
+
+    [Header("Bobbing Animation")]
+    [SerializeField] private float frequency;
+    [SerializeField] private float amplitude;
+
+    private void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (dist > stopDist)
+        {
+            PlayerFollow();
+        }
+    }
+
+    private void PlayerFollow()
+    {
+        if (player != null)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+}

c4dc5efe970712e1c52de7ce1c95db168b5e84a5
Moose <yugihanmaster321@gmail.com>
2024-03-02 16:02:15 -0500

    creation of new player controller

---
+++ Assets/Scripts/Player Scripts/FirstPersonController.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded; // replace isGrounded with a different check for double jumping if double jumping is enabled
+    private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimation && characterController.isGrounded;
+    private bool ShouldShoot => canShoot && Input.GetKeyDown(shootKey);
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canCrouch = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode crouchKey = KeyCode.C;
+    [SerializeField] private KeyCode shootKey = KeyCode.Mouse0;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 6.0f;
+    [SerializeField] private float sprintSpeed = 12.0f;
+    [SerializeField] private float crouchSpeed = 3.0f;
+    
+
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
+
+    [Header("Crouch Parameters")]
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standingHeight = 2.0f;
+    [SerializeField] private float timeToCrouch = 0.25f;
+    [SerializeField] private Vector3 crouchingCenterPoint = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenterPoint = new Vector3(0, 0, 0);
+    private bool isCrouching;
+    private bool duringCrouchAnimation;
+
+    [Header("Shooting Parameters")]
+    private bool canShoot = true;
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+    private Shooting shootScript;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0.0f; // used for look view clamping
+
+    void Awake()
+    {
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+        shootScript = GetComponent<Shooting>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (CanMove)
+        {
+            HandleMovementInput();
+            HandleMouseLook();
+
+            if (canJump) { HandleJump(); }
+            if (canCrouch) { HandleCrouch(); }
+            if (canShoot) { HandleShoot(); }
+
+            ApplyFinalMovements();
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2((isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
+
+    private void HandleJump()
+    {
+        if (ShouldJump) { moveDirection.y = jumpForce; }
+    }
+
+    private void HandleCrouch()
+    {
+        if (ShouldCrouch) { StartCoroutine(CrouchStand()); }
+    }
+
+    private void HandleShoot()
+    {
+        if (ShouldShoot)
+        {
+            shootScript.Shoot(playerCamera.transform);
+            StartCoroutine(CanPlayerShoot());
+        }
+    }
+
+    private void ApplyFinalMovements()
+    {
+        if (!characterController.isGrounded) { moveDirection.y -= gravity * Time.deltaTime; }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        if (isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f)) { yield break; }
+
+        duringCrouchAnimation = true;
+
+        float timeElasped = 0f;
+        float targetHeight = isCrouching ? standingHeight : crouchHeight;
+        float currentHeight = characterController.height;
+        Vector3 targetCenter = isCrouching ? standingCenterPoint : crouchingCenterPoint;
+        Vector3 currentCenter = characterController.center;
+
+        while (timeElasped < timeToCrouch)
+        {
+            characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElasped / timeToCrouch);
+            characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElasped / timeToCrouch);
+            timeElasped += Time.deltaTime;
+            yield return null;
+        }
+
+        characterController.height = targetHeight;
+        characterController.center = targetCenter;
+
+        isCrouching = !isCrouching;
+
+        duringCrouchAnimation = false;
+    }
+
+    // time between the player being able to shoot -- Firerate
+    private IEnumerator CanPlayerShoot()
+    {
+        canShoot = false;
+
+        yield return new WaitForSeconds(0.2f);
+
+        canShoot = true;
+    }
+}

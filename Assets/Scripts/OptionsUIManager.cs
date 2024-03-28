fbadb4bf26e51527ead35e5bd07cda1097efb931
Moose <yugihanmaster321@gmail.com>
2024-03-27 21:52:18 -0400

    added options manager to main menu scene

---
+++ Assets/Scripts/OptionsUIManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsUIManager : MonoBehaviour
+{
+    [Header("UI Functions")]
+    [SerializeField] Slider masterVolumeSlider;
+    [SerializeField] Slider sfxVolumeSlider;
+    [SerializeField] Slider musicVolumeSlider;
+
+    [Header("Player Functions")]
+    [SerializeField] Slider mouseSensitivityXSlider;
+    [SerializeField] Slider mouseSensitivityYSlider;
+
+    public float masterVolume { get; private set; }
+    public float sfxVolume { get; private set; }
+    public float musicVolume { get; private set; }
+
+    public float mouseSensitivityX { get; private set; }
+    public float mouseSensitivityY { get; private set; }
+
+    public void OnMasterVolumeSliderUpdate()
+    {
+        masterVolume = masterVolumeSlider.value;
+    }
+
+    public void OnSFXVolumeSliderUpdate()
+    {
+        sfxVolume = sfxVolumeSlider.value;
+    }
+
+    public void OnMusicVolumeSliderUpdate()
+    {
+        musicVolume = musicVolumeSlider.value;
+    }
+
+    public void OnMouseSensitivityXSliderUpdate() 
+    { 
+        mouseSensitivityX = mouseSensitivityXSlider.value;
+    }
+
+    public void OnMouseSensitivityYSliderUpdate()
+    {
+        mouseSensitivityY = mouseSensitivityYSlider.value;
+    }
+}

fdbeb12ee6fba858e176960f676b14ed30c27be9
Moose <yugihanmaster321@gmail.com>
2024-03-29 04:17:18 -0400

    fixed infinite height jump glitch

--- Assets/Scripts/Creature Scripts/CageController.cs
+++ Assets/Scripts/Creature Scripts/CageController.cs
@@ -8,6 +8,7 @@ public class CageController : MonoBehaviour
     private float stopDist = 4f;
     private float speedUpDist = 10f;
     private float speed = 3f;
+    private float inStopDistRange = 2f;
 
     [SerializeField] private GameObject[] creatures;
 
@@ -29,7 +30,7 @@ public class CageController : MonoBehaviour
         float dist = Vector3.Distance(gameObject.transform.position, player.transform.position);
 
         if (dist > stopDist + speedUpDist) { PlayerFollow(speed * 3f); }
-        else if (dist < speedUpDist + stopDist && dist > stopDist) { PlayerFollow(speed); }
+        else if (dist < speedUpDist + stopDist && dist > stopDist && !(dist <= stopDist + inStopDistRange)) { PlayerFollow(speed); }
     }
 
     private void PlayerFollow(float _speed)

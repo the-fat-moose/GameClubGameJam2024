a08a331119e556412a8961855abff513c8d2d998
Moose <yugihanmaster321@gmail.com>
2024-03-30 18:56:31 -0400

    working on bestiary menu script

--- Assets/Scripts/MainMenuScript/BestiaryMenu.cs
+++ Assets/Scripts/MainMenuScript/BestiaryMenu.cs
@@ -2,20 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BestiaryMenu : MonoBehaviour
 {
+    [Header("Creature Panel Parameters")]
     [SerializeField] GameObject creaturePanel;
     [SerializeField] TMP_Text nameText;
     [SerializeField] TMP_Text timeText;
     [SerializeField] TMP_Text scoreText;
     [SerializeField] TMP_Text infoText;
 
+    private Image tgImage;
+
+    [Header("Creature Button Parameters")]
+    [SerializeField] private Button batButton;
+    [SerializeField] private Button caterpillarButton;
+    [SerializeField] private Button rockButton;
+    [SerializeField] private Button crabButton;
+    [SerializeField] private Button dinoButton;
+
+    [Header("Creature Sprites Locked")]
+    [SerializeField] private Sprite batLocked;
+    [SerializeField] private Sprite caterpillarLocked;
+    [SerializeField] private Sprite rockLocked;
+    [SerializeField] private Sprite crabLocked;
+    [SerializeField] private Sprite dinoLocked;
+
+    [Header("Creature Sprites Unlocked")]
+    [SerializeField] private Sprite batUnlocked;
+    [SerializeField] private Sprite caterpillarUnlocked;
+    [SerializeField] private Sprite rockUnlocked;
+    [SerializeField] private Sprite crabUnlocked;
+    [SerializeField] private Sprite dinoUnlocked;
+
+    [Header("Creature Sprites Highlighted")]
+    [SerializeField] private Sprite batHighlighted;
+    [SerializeField] private Sprite caterpillarHighlighted;
+    [SerializeField] private Sprite rockHighlighted;
+    [SerializeField] private Sprite crabHighlighted;
+    [SerializeField] private Sprite dinoHighlighted;
+
     public List<CreatureData> creaturesCaughtInfo = new List<CreatureData>();
 
     private void Start()
     {
         creaturePanel.SetActive(false);
+
+        SpriteState ss = new SpriteState();
+
+        tgImage.sprite = batLocked;
+        batButton.targetGraphic = tgImage;
+        ss.highlightedSprite = batLocked;
+        batButton.spriteState = ss;
+
+        tgImage.sprite = caterpillarLocked;
+        caterpillarButton.targetGraphic = tgImage;
+        ss.highlightedSprite = caterpillarLocked;
+        caterpillarButton.spriteState = ss;
+
+        tgImage.sprite = rockLocked;
+        rockButton.targetGraphic = tgImage;
+        ss.highlightedSprite = rockLocked;
+        rockButton.spriteState = ss;
+
+        tgImage.sprite = dinoLocked;
+        dinoButton.targetGraphic = tgImage;
+        ss.highlightedSprite = dinoLocked;
+        dinoButton.spriteState = ss;
+
+        tgImage.sprite = crabLocked;
+        crabButton.targetGraphic = tgImage;
+        ss.highlightedSprite = crabLocked;
+        crabButton.spriteState = ss;
     }
 
     public void OnCaterpillarButtonClick()
